7830ea0bd7b9e3210b3acbde145fbe3b124267bb
Dastan Ahmet <dastanahmet01@gmail.com>
2019-11-13 06:01:14 +0600

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'menu',
+    component: () => import('@/views/Menu')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/game',
+    name: 'game',
+    component: () => import('@/views/Game')
+  },
+  {
+    path: '/result',
+    name: 'result',
+    component: () => import('@/views/Result')
   }
 ]
 

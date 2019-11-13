6aa3566b9e705df996964f41ead554b36d59041f
Dastan Ahmet <dastanahmet01@gmail.com>
2019-11-13 06:00:58 +0600

    Add game page

---
+++ src/views/Game.vue
@@ -0,0 +1,26 @@
+<template>
+  <DefaultLayout>
+    <h1>Game</h1>
+    <p>{{ guesser }} {{ inProgress }}</p>
+    <button @click="onClick">stop game</button>
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/Default'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  components: { DefaultLayout },
+  computed: {
+    ...mapGetters(['guesser', 'inProgress'])
+  },
+  methods: {
+    ...mapActions(['stopGame']),
+    onClick () {
+      this.stopGame()
+      this.$router.history.push('/')
+    }
+  }
+}
+</script>

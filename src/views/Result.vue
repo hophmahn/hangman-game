4f4a251c00e16882b9066bb0058d15c085f8e3da
Dastan Ahmet <dastanahmet01@gmail.com>
2019-11-13 06:01:03 +0600

    Add result page

---
+++ src/views/Result.vue
@@ -0,0 +1,26 @@
+<template>
+  <DefaultLayout>
+    <h1>You {{ result }}!</h1>
+    <Button size="md" :onClick="onButtonPress">Go to menu</Button>
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/Default'
+import Button from '@/components/Button'
+
+export default {
+  components: {
+    DefaultLayout,
+    Button
+  },
+  props: {
+    result: String
+  },
+  methods: {
+    onButtonPress (event) {
+      this.$router.history.push('/')
+    }
+  }
+}
+</script>

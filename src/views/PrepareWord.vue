5bf94029b9bbd139644c7978040aa134c93e92f0
Dastan Ahmet <dastanahmet01@gmail.com>
2019-11-13 14:50:23 +0600

    Add view for word preparation page

---
+++ src/views/PrepareWord.vue
@@ -0,0 +1,16 @@
+<template>
+  <DefaultLayout>
+    <h1>Set the word</h1>
+    <input type="text" placeholder="Type some word..."/>
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/Default'
+
+export default {
+  components: {
+    DefaultLayout
+  }
+}
+</script>
